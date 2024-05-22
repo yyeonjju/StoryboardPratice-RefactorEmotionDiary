a5d06de555059dbdaa54db08bff24fceed4d2543
yyeonjju <llilyha123@gmail.com>
2024-05-22 17:20:55 +0900

    Add : UserDefaults 사용해서 데이터 저장, 삭제 기능 추가

--- SesacAssignment0517/ViewControllers/SecondEmotionDiaryViewController.swift
+++ SesacAssignment0517/ViewControllers/SecondEmotionDiaryViewController.swift
@@ -11,7 +11,9 @@ class SecondEmotionDiaryViewController: UIViewController {
     
     @IBOutlet var buttonList: [UIButton]!
     @IBOutlet var labelList: [UILabel]!
-    let emotionNameList = ["행복해", "사랑해", "좋이해", "당황해", "속상해", "우울해", "심심해", "행복해", "깜찍해"]
+    @IBOutlet var resetDataButton: UIButton!
+    
+    let emotionNameList = ["행복해", "사랑해", "좋아해", "당황해", "속상해", "우울해", "심심해", "행복해", "깜찍해"]
     let buttonImageList = [
         UIImage(named: "slime1"),
         UIImage(named: "slime2"),
@@ -23,16 +25,20 @@ class SecondEmotionDiaryViewController: UIViewController {
         UIImage(named: "slime8"),
         UIImage(named: "slime9"),
     ]
-    var count = Array(repeating: 0, count: 9)
-    
-    
+    var count : [Int] = Array(repeating: 0, count: 9)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+        setEmotionCountData()
 
+    }
+    
+    func configureUI () {
         labelList.enumerated()
             .forEach{
-                $0.element.text = "\(emotionNameList[$0.offset]) 0번"
+                $0.element.text = makeLabelText(emotionName: emotionNameList[$0.offset], countString: "0")
             }
         
         buttonList.enumerated()
@@ -44,13 +50,45 @@ class SecondEmotionDiaryViewController: UIViewController {
                 $0.element.imageView?.contentMode = .scaleAspectFit
   
             }
-
+        
+        resetDataButton.setTitle("횟수 리셋하기", for: .normal)
+        resetDataButton.backgroundColor = .blue
+        resetDataButton.layer.cornerRadius = 10
+        resetDataButton.setTitleColor(.white, for: .normal)
     }
     
+    func makeLabelText(emotionName:String, countString : String) -> String{
+        return "\(emotionName) \(countString)번"
+    }
+    
+    func setEmotionCountData() {
+        if let emotionCountData = UserDefaults.standard.array(forKey: "emotionCount") {
 
+            self.count = emotionCountData as! [Int]
+            self.count.enumerated().forEach{
+                labelList[$0.offset].text = makeLabelText(emotionName: emotionNameList[$0.offset], countString: String($0.element))
+            }
+        }
+
+    }
  
     @IBAction func emotionButtonTapped(_ sender: UIButton) {
         count[sender.tag] += 1
-        labelList[sender.tag].text = "\(emotionNameList[sender.tag]) \(count[sender.tag])번"
+        labelList[sender.tag].text = makeLabelText(emotionName: emotionNameList[sender.tag], countString: String(count[sender.tag]))
+        
+        //감정 버튼 각각 몇 번 눌렸는지 저장
+        UserDefaults.standard.set(count, forKey: "emotionCount")
     }
+    
+    @IBAction func resetCountData(_ sender: UIButton) {
+        UserDefaults.standard.removeObject(forKey: "emotionCount")
+
+        count = Array(repeating: 0, count: 9)
+        labelList.enumerated()
+            .forEach{
+                $0.element.text = makeLabelText(emotionName: emotionNameList[$0.offset], countString: "0")
+            }
+    }
+
 }
+
